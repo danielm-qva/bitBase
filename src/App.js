3cb1158b4c471e44b654c8c528ea156bee34232b
danielm-qva <jmastrapa96@gamil.com>
2021-04-13 16:08:01 -0400

    Al dia

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Barra from './component/NavBar/NavBar.js';
-import { Jumbotron, Container } from 'react-bootstrap';
+import Footer from './component/Footer/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-      <Barra />
+    <Router>
+    <div>
+        <Barra/>
+        <Footer/>
+        <Switch>
+            <Route path="/about">
+            </Route>
+            <Route path="/users">
+            </Route>
+            <Route path="/">
+            </Route>
+        </Switch>
+    </div>
+</Router>
+      
   );
 }
 
