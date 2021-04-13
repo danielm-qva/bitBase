b5ec6316fa60faeade4d0ce1bd39981803a03e87
danielm-qva <jmastrapa96@gamil.com>
2021-04-13 16:01:07 -0400

    List

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import { Jumbotron, Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-
       <Barra />
-      <Jumbotron className="backColor" fluid>
-        <Container>
-          <h1>Fluid jumbotron</h1>
-          <p>
-            This is a modified jumbotron that occupies the entire horizontal space of
-            its parent.
-    </p>
-        </Container>
-      </Jumbotron>
-    </div>
   );
 }
 
