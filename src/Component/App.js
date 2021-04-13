a0480ac586530fd2a4381b3f71e6df64c90dd641
danielm-qva <jmastrapa96@gamil.com>
2021-04-13 11:07:49 -0400

    Inicion del Proyecto

---
+++ src/Component/App.js
@@ -0,0 +1,126 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+
+      <nav class="navbar navbar-expand-lg bitheadblack navbar-dark">
+        <div className="container pl-xl-0">
+          <a className="navbar-brand active" href="https://bitbase.es/"><img src="https://bitbase.es/images/logo.svg" id="logoImage" alt="bitbase" /></a>
+          <section className="right-side">
+            <div className="right-news"> <a href="https://news.bitbase.es/" target="_blank"> <img src="https://bitbase.es/images/newsorange.svg" alt="bitcoin bitbae" /> <span>NEWS</span></a> </div>
+          </section>
+          <button className="navbar-toggler headbtn" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul className="navbar-nav bitfacts-list ml-lg-5">
+              <li className="nav-item mx-lg-2 mx-xl-3">
+                <a className="nav-link active" href="https://bitbase.es/">HOME</a>
+              </li>
+              <li className="nav-item mx-lg-1  mx-xl-3">
+                <a className="nav-link mx-lg-1" href="https://bitbase.es/comprar-bitcoin">COMPRAR</a>
+              </li>
+              <li className="nav-item mx-lg-1 mx-xl-3">
+                <a className="nav-link mx-lg-1" href="https://bitbase.es/vender-bitcoin">VENDER</a>
+              </li>
+              <li className="nav-item dropdown mx-xl-3">
+                <a className="nav-link dropdown-toggle text-center mx-xl-3" href="#" id="navbardrop" data-toggle="dropdown">
+                  TIENDAS
+          </a>
+                <div className="dropdown-menu tiedrop">
+                  <a className="dropdown-item" href="https://bitbase.es/tiendas-bitcoin">Ver todas las tiendas</a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/madrid-princesa">
+                    Tienda Bitcoin Madrid Centro </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/madrid">
+                    Tienda Bitcoin Madrid </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/barcelona">
+                    Tienda Bitcoin Barcelona </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/barcelona-eixample">
+                    Tienda Bitcoin Barcelona Centro </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/barcelona-balmes">
+                    Tienda Bitcoin Barcelona Balmes </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/valencia">
+                    Tienda Bitcoin Valencia </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/bilbao">
+                    Tienda Bitcoin Bilbao </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/sevilla">
+                    Tienda Bitcoin Sevilla </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/malaga">
+                    Tienda Bitcoin Malaga </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/tenerife">
+                    Tienda Bitcoin Tenerife </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/zaragoza">
+                    Tienda Bitcoin Zaragoza </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/coruña">
+                    Tienda Bitcoin Coruña </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/mallorca">
+                    Tienda Bitcoin Mallorca </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/valladolid">
+                    Tienda Bitcoin Valladolid </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/vitoria">
+                    Tienda Bitcoin Vitoria </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/girona">
+                    Tienda Bitcoin Girona </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/vilanova-i-la-geltru">
+                    Tienda Bitcoin Vilanova </a>
+                  <a className="dropdown-item" href="https://bitbase.es/comprar-bitcoin/hospitalet">
+                    Tienda Bitcoin Hospitalet </a>
+                </div>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle text-center mx-xl-1" href="#" id="navbardrop" data-toggle="dropdown">
+                  CAJEROS
+          </a>
+                <div className="dropdown-menu tiedrop">
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin">Todo los cajeros Bitcoin</a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#barcelona">Cajeros Bitcoin Barcelona </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#madrid">Cajeros Bitcoin Madrid </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#valencia">Cajeros Bitcoin Valencia </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#bilbao">Cajeros Bitcoin Bilbao </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#sevilla">Cajeros Bitcoin Sevilla </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#malaga">Cajeros Bitcoin Malaga </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#torremolinos">Cajeros Bitcoin Torremolinos </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#mallorca">Cajeros Bitcoin Mallorca </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#tenerife">Cajeros Bitcoin Tenerife </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#zaragoza">Cajeros Bitcoin Zaragoza </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#coruña">Cajeros Bitcoin Coruña </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#murcia">Cajeros Bitcoin Murcia </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#valladolid">Cajeros Bitcoin Valladolid </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#vitoria">Cajeros Bitcoin Vitoria </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#girona">Cajeros Bitcoin Girona </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#vilanova">Cajeros Bitcoin Vilanova </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#reus">Cajeros Bitcoin Reus </a>
+                  <a className="dropdown-item" href="https://bitbase.es/cajeros-bitcoin#la_jonquera">Cajeros Bitcoin La jonquera </a>
+                </div>
+              </li>
+              <li className="nav-item mx-lg-1 mx-xl-3" id="bitnews-nav">
+                <a className="nav-link" href="https://news.bitbase.es/" target="_blank"><img src="https://bitbase.es/images/newsorange.svg" alt="bitbase" /> BITNEWS</a>
+              </li>
+              <li className="nav-item ml-4" id="nav-lang">
+                <ul className="navbar-nav mx-lg-3 es-enlist">
+                  <li className="nav-item">
+                    <a className="nav-link ml-lg-n3" href="https://bitbase.es/prestamo-criptomonedas"> BITCREDIT </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item" id="nav-lang">
+                <ul className="navbar-nav mx-lg-3 es-enlist">
+                  <li className="nav-item">
+                    <a className="nav-link mr-md-0 active">ES <span className="text-change ml-n2 d-none d-lg-inline">/</span> <span className="text-change ml-n2 d-inline d-lg-none">-</span></a>
+                  </li>
+                  <li className="nav-item">
+                    <a id="EN" className="nav-link ml-lg-n3" href="https://bitbase.es/language?lang=EN&page=/" onclick="linkUpdate(this,&quot;EN&quot;,&quot;https://bitbase.es/hashgrab&quot;);">EN</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+  </nav>
+
+  );
+}
+
+export default App;
